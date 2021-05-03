4635b5337a5da6be380568c567f11adc21b3a4ca
eddiechacha <edwinchalas2023@u.northwestern.edu>
2021-05-03 13:38:04 -0500

    please work

---
+++ Notes.R
@@ -0,0 +1,107 @@
+#Notes:
+
+#Stratify the data (because it'll take too long to run if not)
+#5%, 20k rows
+
+#NOT RANDOM FOREST OR BOOSTED TREE (takes too long)
+#Neural net - Edwin
+#Elastic net - Lauren
+#Random forest - Preston
+#Boosted tree - Josh
+
+#Predicting the price of a car based off of variables
+
+# Load package(s) ----
+library(tidyverse)
+library(tidymodels)
+
+# Seed
+set.seed(2021)
+
+load(file = "data/vehicles.rda")
+vehicles <- vehicles %>% 
+  clean_names() %>%
+  select(-c(id, url, region_url, vin, image_url, description)) %>%
+  mutate(state_region = 
+           case_when(
+             state %in% c("wv", "va", "ky", "nc", "sc", "tn", "ar", "la", "al", "ms", "ga", "fl") ~ "southeast", 
+             state %in% c("me", "vt", "nh", "ma", "ct", "ri", "ny", "nj", "pa", "de", "md", "dc") ~ "northeast", 
+             state %in% c("nd", "sd", "mn", "wi", "ia", "ne", "ks", "mo", "il", "in", "mi", "oh") ~ "midwest", 
+             state %in% c("ok", "tx", "nm", "az") ~ "southwest", 
+             state %in% c("ak", "hi", "wa", "or", "id", "mt", "wy", "co", "ut", "nv", "ca") ~ "west"
+           )
+  ) %>%
+  mutate(condition = factor(condition)) %>% 
+  mutate(cylinders = factor(cylinders)) %>% 
+  mutate(fuel = factor(fuel)) %>% 
+  mutate(title_status = factor(title_status)) %>% 
+  mutate(transmission = factor(transmission)) %>% 
+  mutate(drive = factor(drive)) %>% 
+  mutate(size = factor(size)) %>% 
+  mutate(type = factor(type)) %>% 
+  mutate(paint_color = factor(paint_color)) %>% 
+  mutate(state_region = factor(state_region)) %>% 
+  mutate(manufacturer = factor(manufacturer)) %>%
+  filter(price > 1000 & price < 100000) %>% 
+  mutate(price = log10(price)) %>%
+  filter(odometer < 300000) %>% 
+  filter(year > 1993)
+
+save(vehicles, file = "data/vehicles.rda")
+
+vehicles_strat <- vehicles %>% 
+  stratified("price", 0.05)
+save(vehicles, file = "data/vehicles_strat.rda")
+
+vehicle_split <- vehicle_data %>%
+  initial_split(prop = .8, strata = price)
+
+vehicle_train <- training(vehicle_split)
+
+vehicle_test <- testing(vehicle_split)
+
+vehicle_recipe <- recipe(price ~ ., data = vehicle_strat) %>%
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
+# load required objects ----
+#load("model_info/wildfires_setup.rda")
+
+#tuning
+model_folds <- vfold_cv(vehicle_train, v = 5, repeats = 3)
+
+# Define model ----
+knn_model <- nearest_neighbor(
+  mode = "classification",
+  neighbors = tune()
+) %>%
+  set_engine("kknn")
+
+# # check tuning parameters
+# parameters(knn_model)
+
+# set-up tuning grid ----
+knn_params <- parameters(knn_model) %>%
+  update(neighbors = neighbors(range = c(1,40)))
+
+# define grid
+knn_grid <- grid_regular(knn_params, levels = 15)
+
+# workflow ----
+knn_workflow <- workflow() %>%
+  add_model(knn_model) %>%
+  add_recipe(vehicle_recipe)
+
+class_metrics <- metric_set(precision, recall, sensitivity, specificity, f_meas, accuracy, roc_auc)
+
+# Tuning/fitting ----
+knn_tune <- knn_workflow %>%
+  tune_grid(
+    resamples = model_folds,
+    grid = knn_grid,
+    metrics = class_metrics
+  )
+# Write out results & workflow
+save(knn_tune, knn_workflow, file = "model_info/knn_tune.rda")
+
+knn_tune
